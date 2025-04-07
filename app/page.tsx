6cd0f99501dedfc37b5cd2737402ab7b7ecebc04
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-04-07 12:31:46 +0800

    add input

--- app/page.tsx
+++ app/page.tsx
@@ -28,13 +28,41 @@ export default function Home() {
   return (
     <div className="items-center justify-items-center px-6 py-12 md:p-14 h-full md:h-full bg-[url(/Images/background.png)] bg-center bg-cover">
       <div className="flex flex-col gap-8 w-full md:w-lg">
-        <header className="flex flex-col gap-2 px-6">
-          <h1 className="text-4xl font-semibold text-gray-700">
-            UI/UX Designer
-          </h1>
-          <p className="text-md font-medium text-indigo-600">
-            + I do code sometimes
-          </p>
+        <header className="flex flex-col md:flex-row gap-8 items-center justify-between">
+          <div className="flex flex-col gap-2 px-6">
+            <h1 className="text-4xl font-semibold text-gray-700">
+              UI/UX Designer
+            </h1>
+            <p className="text-md font-medium text-indigo-600">
+              + I do code sometimes
+            </p>
+          </div>
+          <div className="flex flex-row gap-8 md:gap-2">
+            <a
+              href="https://dribbble.com/badrulhanif"
+              target="blank"
+              rel="noreferrer"
+              className="text-gray-500 hover:text-white p-2 rounded-full hover:bg-pink-400"
+            >
+              <Dribbble className="stroke-[1.5] " />
+            </a>
+            <a
+              href="https://github.com/badrulhanif"
+              target="blank"
+              rel="noreferrer"
+              className="text-gray-500 hover:text-white p-2 rounded-full hover:bg-black"
+            >
+              <GitHub className="stroke-[1.5]" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/badrul-hanif-b01471196/"
+              target="blank"
+              rel="noreferrer"
+              className="text-gray-500 hover:text-white p-2 rounded-full hover:bg-blue-400"
+            >
+              <Linkedin className="stroke-[1.5]" />
+            </a>
+          </div>
         </header>
         <main className="flex flex-col border-1 border-gray-200 p-6 gap-8 rounded-3xl shadow-xl bg-white">
           <div className="flex flex-col gap-4">
@@ -126,59 +154,47 @@ export default function Home() {
             </div>
           </div>
         </main>
-        <footer className="flex flex-col md:flex-row gap-8 items-center justify-between">
-          <button
-            onClick={handleMailTo}
-            className="font-medium text-white  px-5 py-3 w-full md:w-fit cursor-pointer rounded-full bg-indigo-500 hover:bg-indigo-800 "
-          >
-            Collaborate with me
-          </button>
-          <div className="flex flex-row gap-8 md:gap-2">
-            <a
-              onClick={handleCopyEmail}
-              className="relative inline-block text-gray-500 hover:text-white p-2 rounded-full hover:bg-black group" // Added `group` class here
+        <footer className="flex flex-col border-1 border-gray-200 p-6 gap-8 rounded-3xl shadow-xl bg-white">
+          <p className="text-md font-semibold text-gray-600">
+            COLLABORATE WITH ME
+          </p>
+          <form onSubmit={handleMailTo} className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="email"
+                className="text-md font-medium text-gray-400"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example@example.com"
+                required
+                className="text-md font-medium text-gray-400 border-1 border-gray-200 p-3 rounded-xl "
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="email"
+                className="text-md font-medium text-gray-400"
+              >
+                Messages
+              </label>
+              <textarea
+                placeholder="Your message here..."
+                required
+                className="text-md font-medium text-gray-400 border-1 border-gray-200 h-32 p-3 rounded-xl overflow-y-auto resize-none"
+              />
+            </div>
+            <button
+              type="submit"
+              className="font-medium text-white  px-5 py-3 w-full cursor-pointer rounded-full bg-indigo-500 hover:bg-indigo-800 "
             >
-              <Mail className="stroke-[1.5]" />
-
-              {/* Show Email on Hover (only visible when not copied) */}
-              {!copied && (
-                <span className="absolute -top-12 left-1/2 -translate-x-1/2 text-center p-2 w-52 rounded-full text-sm text-white bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  hanifbaharuddin@gmail.com
-                </span>
-              )}
-
-              {/* Show 'Copied to clipboard!' on Click */}
-              {copied && (
-                <span className="absolute -top-12 left-1/2 -translate-x-1/2 text-center p-2 w-40 rounded-full text-sm text-white bg-black/50 opacity-100 transition-opacity duration-300">
-                  Copied to clipboard!
-                </span>
-              )}
-            </a>
-            <a
-              href="https://dribbble.com/badrulhanif"
-              target="blank"
-              rel="noreferrer"
-              className="text-gray-500 hover:text-white p-2 rounded-full hover:bg-pink-400"
-            >
-              <Dribbble className="stroke-[1.5] " />
-            </a>
-            <a
-              href="https://github.com/badrulhanif"
-              target="blank"
-              rel="noreferrer"
-              className="text-gray-500 hover:text-white p-2 rounded-full hover:bg-black"
-            >
-              <GitHub className="stroke-[1.5]" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/badrul-hanif-b01471196/"
-              target="blank"
-              rel="noreferrer"
-              className="text-gray-500 hover:text-white p-2 rounded-full hover:bg-blue-400"
-            >
-              <Linkedin className="stroke-[1.5]" />
-            </a>
-          </div>
+              Let's ship this!
+            </button>
+          </form>
         </footer>
       </div>
     </div>
