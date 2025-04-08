18da976016038e422441f88bd8f9a69ac0d34227
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-04-08 14:32:15 +0800

    fix close message after 3s

--- app/page.tsx
+++ app/page.tsx
@@ -256,7 +256,7 @@ export default function Home() {
           </form>
         </footer>
         {sent && (
-          <p className="absolute bottom-20 left-1/2 transform -translate-x-1/2 text-center px-4 py-2 rounded-full text-sm text-white bg-black/50 duration-300">
+          <p className="absolute bottom-20 left-1/2 transform -translate-x-1/2 text-center px-4 py-2 rounded-full text-sm text-white bg-black/50 transition-opacity opacity-0 duration-300">
             Your idea has set sail 🚀
           </p>
         )}
