e28bc650efc35a41772410e45ff48d944674ba27
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-04-09 15:11:27 +0800

    fix logo

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: Request) {
       subject: "Your idea has set sail 🚀",
       html: `<div style="max-width: 600px; margin: 0 auto;">
   <div style="text-align: center; margin-bottom: 24px;">
-    <img src="https://picsum.photos/200/300" alt="Logo" width="96" height="96" style="margin-bottom: 12px;" />
+    <img src="https://www.badrulhanif.com/Images/logo.png" alt="Logo" width="96" height="96" style="margin-bottom: 12px;" />
     <h1 style="font-size: 24px; color: #4A4E5A; margin-bottom: 12px;">We heard you!</h1>
     <p style="font-size: 16px; color: #5B616E;">Thank you for reaching out. Your idea sounds awesome, and I'm excited about the possibility of working together!</p>
   </div>
