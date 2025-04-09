3bde4b447ebd22b1c9c67ac9c527bff50cd81599
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-04-09 16:28:34 +0800

    fix email template

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -11,7 +11,20 @@ export async function POST(request: Request) {
       from: "no-reply <noreply@badrulhanif.com>",
       to: ["hanifbaharuddin@gmail.com"], // must be an array
       subject: "New Collaboration Request",
-      html: `<p><strong>From:</strong> ${email}</p><p>${message}</p>`,
+      html: `<div style="max-width: 600px; margin: 0 auto;">
+  <div style="text-align: center; margin-bottom: 24px;">
+    <img src="https://www.badrulhanif.com/Images/cat-icon.png" alt="Logo" width="96" height="96" style="margin-bottom: 12px;" />
+    <h1 style="font-size: 24px; color: #4A4E5A; margin-bottom: 12px;">Hey Badrul!</h1>
+    <p style="font-size: 16px; color: #5B616E;">Someone has request to collaborate with you</p>
+  </div>
+
+  <div style="padding: 12px; margin-bottom: 24px; border: 1px solid #ddd; border-radius: 12px; background: #f9f9f9;">
+    <p style="font-size: 16px; color: #5B616E; font-weight: 600;">Collaboration request:</p>
+    <p style="font-size: 16px; color: #5B616E;">From: ${email}</p>
+    <p style="font-size: 16px; color: #5B616E;">${message}</p>
+  </div>
+</div>
+`,
     });
 
     const copyToUser = await resend.emails.send({
@@ -30,9 +43,10 @@ export async function POST(request: Request) {
     <p style="font-size: 16px; color: #5B616E;">${message}</p>
   </div>
 
-  <p style="font-size: 16px; color: #5B616E; margin-bottom: 24px;">Let’s build something great together!</p>
+  <p style="font-size: 16px; color: #5B616E;">Let’s build something great together!</p>
+  <p style="font-size: 16px; color: #5B616E; margin-bottom: 48px;">Any inquiries, email to:<br />hanifbaharuddin@gmail.com</p>
   
-  <p style="font-size: 16px; text-align: center; color: #1a73e8; text-decoration: none; display: block;">
+  <p style="font-size: 16px; text-align: center; color: #1a73e8; margin-bottom: 24px; text-decoration: none; display: block;">
     <a href="https://dribbble.com/badrulhanif" style="margin: 0 10px;">Dribbble</a>
     <a href="https://github.com/badrulhanif" style="margin: 0 10px;">GitHub</a>
     <a href="https://www.linkedin.com/in/badrul-hanif-b01471196/" style="margin: 0 10px;">LinkedIn</a>
