5c68c102c17724ba7978a66dd2a16ef4d4eeb007
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-04-14 12:19:29 +0800

    add custome messages API call

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -2,11 +2,18 @@ import { promises as fs } from "fs";
 import path from "path";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const acceptHeader = request.headers.get("accept");
   const imagesDirectory = path.join(process.cwd(), "public/Images");
   try {
     const filenames = await fs.readdir(imagesDirectory); // Read filenames in the images folder
-    return NextResponse.json(filenames); // Send filenames as JSON
+    if (acceptHeader && acceptHeader.includes("text/html")) {
+      return new NextResponse(
+        "This endpoint serves image file names as JSON. Please use it via an API call."
+      );
+    }
+
+    return NextResponse.json(filenames); // Send filenames as JSON to the frontend
   } catch (error) {
     console.error("Error reading images directory:", error);
     return NextResponse.json(
